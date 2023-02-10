dc563da16faa634c85989a9424a11a845d5b4203
Bongbang4862 <luis.cordova.olavarria@gmail.com>
2023-02-10 02:17:56 -0300

    hice cambios en el codigo de javasc

--- resources/views/connect/template.blade.php
+++ resources/views/connect/template.blade.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{url('/static/css/dashboard.css')}}">
-    <link rel="stylesheet" href="{{url('/css/bootstrap.min.css')}}"> 
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/455de5f3f1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{url('/static/css/dashboard.css')}}">
+
     
     @section('css')
    
@@ -18,54 +19,64 @@
 
 </head>
 <body>
-    <div class="menu-dashboard">
+    <div class="row">
+        <div  class="col-3">
+            <div class="menu-dashboard">
 
-        <div class="top-menu">
-            <div class="logo">
-                <img src="/static/img/logo.jpg" alt="">
-                <span>Entretenimiento</span>
-            </div>
-            <div class="toggle">
-                <i class="fa-solid fa-toggle-on"></i>
+                <div class="top-menu">
+                    <div class="logo">
+                        <img src="/static/img/logo.jpg" alt="">
+                        <span>Bienvenido</span>
+                    </div>
+                    <div class="toggle">
+                        <i class="fa-solid fa-toggle-on"></i>
+                    </div>
+                </div>
+        
+                <div class="input-search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <input type="text" class="input" placeholder="Buscar">
+                </div>
+        
+                <div class="menu">
+                    <a href="{{url('/admin/dashboard')}}">
+                        <div class="enlace">
+                            <i class="fa-solid fa-gauge-high"></i>
+                            <span>Dashboard</span>
+                        </div>
+                    </a>
+                    <a href="{{url('/admin/movies')}}">
+                        <div class="enlace">
+                            <i class="fa-solid fa-film"></i>
+                            <span>Peliculas</span>
+                        </div>
+                    </a> 
+                    <div class="enlace">
+                        <i class="fa-solid fa-tv"></i>
+                        <span>Series</span>
+                    </div>
+                    <div class="enlace">
+                        <i class="fa-solid fa-basket-shopping"></i>
+                        <span>Pedidos</span>
+                    </div>
+                    <div class="enlace">
+                        <i class="fa-solid fa-gears"></i>
+                        <span>Configuracion</span>
+                    </div>
+                </div>
+        
             </div>
         </div>
-
-        <div class="input-search">
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <input type="text" class="input" placeholder="Buscar">
+        <div class="col-9">
+            @section('contenido')
+   
+                 {{-- para mostrar el contenido --}}
+            @show
         </div>
-
-        <div class="menu">
-            <div class="enlace">
-                <i class="fa-solid fa-gauge-high"></i>
-                <span>Dashboard</span>
-            </div>
-            <div class="enlace">
-               <a href="{{url('/admin/movies')}}">
-                    <i class="fa-solid fa-film"></i>
-                    <span>Peliculas</span>
-                </a> 
-            </div>
-            <div class="enlace">
-                <i class="fa-solid fa-tv"></i>
-                <span>Series</span>
-            </div>
-            <div class="enlace">
-                <i class="fa-solid fa-basket-shopping"></i>
-                <span>Pedidos</span>
-            </div>
-            <div class="enlace">
-                <i class="fa-solid fa-gears"></i>
-                <span>Configuracion</span>
-            </div>
-        </div>
-
     </div>
+    
    
-    @section('contenido')
-   
-    {{-- para mostrar el contenido --}}
-    @show 
+     
 
     {{-- <script src="{{url('/css/bootstrap.min.js')}}"></script> --}}
     <script src="{{url('static/js/api.js')}}"></script>
