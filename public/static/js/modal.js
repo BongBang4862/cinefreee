bc92a0f592b82289c505a6164f0fccbf4cbc2640
BongBang <luis.cordova.olavarria>
2023-02-17 16:58:46 -0300

    se agrego una alerta al modal

--- public/static/js/modal.js
+++ public/static/js/modal.js
@@ -9,52 +9,45 @@ document.addEventListener('DOMContentLoaded',function (e) {
     const link = document.getElementById('addLink');
     modal.addEventListener('click',function(e){
         hideModal(modal);
+        alert('probando')
     })
     btn = [btnagregarmovie ,btnagregarlink,btnagregarcategory]
-    btnagregarlink.addEventListener('click',(e)=>{
-        e.preventDefault()
-        if (link.style.display=='none'){
-            showModal(link);
-        }else{
-            
-            hideModal(link);
-        }
-    })
-//     btn.forEach(element => { element.addEventListener('click',function(e){
-//             e.preventDefault();
-//             switch (element) {
-//                 case btnagregarmovie:
-//                     if (modal.style.display=='none'){
-//                         showModal(modal);
-//                     }else{
-                       
-//                     }
-//                     break;
-//                 case btnagregarlink:
-//                     if (link.style.display=='none'){
-//                         showModal(link);
-//                     }else{
-                        
-//                         hideModal(link);
-//                     }
-//                     break;
-//                 case btnagregarcategory:
-//                     if (category.style.display=='none'){
-//                         showModal(category);
-//                     }else{
-                        
-//                         hideModal(category);
-//                     }
-//                     break;
-            
-//                 default:
-//                     break;
-//             }
-//         })
-        
-//     });
 
-// })
+    btn.forEach(element => { element.addEventListener('click',function(e){
+            e.preventDefault();
+            switch (element) {
+                case btnagregarmovie:
+                    if (modal.style.display=='none'){
+                        showModal(modal);
+                    }else{
+                       
+                    }
+                    break;
+                case btnagregarlink:
+                    if (link.style.display=='none'){
+                        showModal(link);
+                    }else{
+                        
+                        hideModal(link);
+                    }
+                    break;
+                case btnagregarcategory:
+                    if (category.style.display=='none'){
+                        showModal(category);
+                    }else{
+                        
+                        hideModal(category);
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+        })
+        
+    });
+
+})
 
 function showModal(modal) {
     modal.style.display = "block"
